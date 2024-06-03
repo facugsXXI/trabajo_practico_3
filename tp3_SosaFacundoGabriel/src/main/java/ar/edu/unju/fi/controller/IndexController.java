73e3dfdf83289ffa34bc09ce17dba4eb7df0cfee
SOSA <SOSA@DESKTOP-V4PQRV0>
2024-06-03 19:56:22 -0300

    Comenzando a rehacer el TP3 teniendo en cuenta los comentarios.
    Creando el controlador IndexController, utilizando GetMapping

---
+++ tp3_SosaFacundoGabriel/src/main/java/ar/edu/unju/fi/controller/IndexController.java
@@ -0,0 +1,14 @@
+package ar.edu.unju.fi.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+	
+	@GetMapping("/principal")
+	public String getIndexPage() {
+		return"index";
+	}
+}
